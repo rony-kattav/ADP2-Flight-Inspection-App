c917cc8bb22a4e85b0a3a589168b7654635bb438
unknown <shakedor22@gmail.com>
2021-03-29 15:34:19 +0300

    files from Shaked's computer 29.3

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADP2_Flight_Inspection_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ADP2ViewModel vm;
+        bool csvchanged = false;
+        bool xmlchanged = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new MediaPlayerViewModel(new MediaPlayerModel());
+            //DataContext = vm;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            csvchanged = true;
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            xmlchanged = true;
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+           // vm.modelStart();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayerView mp = new MediaPlayerView();
+            mp.Show();
+            this.Close();
+        }
+    }
+}
---
+++ MediaPlayer.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ADP2_Flight_Inspection_App
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MediaPlayerView : Window
+    {
+        private MediaPlayerViewModel vm;
+
+        public MediaPlayerView()
+        {
+            InitializeComponent();
+            //vm = new MediaPlayerViewModel(new IADP2myModel());
+            //DataContext = vm;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
+        }
+
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem cbi = (ComboBoxItem)speedBox.SelectedItem;
+            string newSpeed = cbi.Content.ToString();
+            string [] arr = newSpeed.Split(' ');
+            vm.VM_Speed = Double.Parse(arr[1]);
+        }
+    }
+}
