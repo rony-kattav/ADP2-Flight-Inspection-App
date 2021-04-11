fcc02d1b5699ed36316636985e1fb53414cd1712
unknown <shakedor22@gmail.com>
2021-04-11 14:44:33 +0300

    helper class for DLL + changes in popup model

--- PopupDetectionModel.cs
+++ PopupDetectionModel.cs
@@ -12,24 +12,6 @@ using System.IO;
 namespace ADP2_Flight_Inspection_App
 {
 
-
-    static class NativeMethods
-    {
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr LoadLibrary(string dllToLoad);
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool FreeLibrary(IntPtr hModule);
-
-
-
-    }
-
-
     public class PopupDetectionModel : IADP2Model
     {
         private bool isStop;
@@ -62,6 +44,10 @@ namespace ADP2_Flight_Inspection_App
                         anomalyTime = int.Parse(splitstr[2]);
                     }
                 }
+                else
+                {
+                    anomalyTime = -1;
+                }
             }
         }
 
@@ -86,16 +72,7 @@ namespace ADP2_Flight_Inspection_App
                 if (String.Compare(dllPath, value) != 0)
                 {
                     dllPath = value;
-                    anomalyReport.Clear();
-                    getDLLAnomalyReport();
-                    while (time > anomalyTime && nextAnomalyIndex < (anomalyreportSize - 1))
-                    {
-                        nextAnomalyIndex++;
-                        nextAnomaly = anomalyReport[nextAnomalyIndex];
-                        string[] splitstr = nextAnomaly.Split('-');
-                        anomalyTime = int.Parse(splitstr[2]);
-                    }
-                    
+                    connect();                 
                 }
 
             }
@@ -203,258 +180,64 @@ namespace ADP2_Flight_Inspection_App
 
         }
 
-        private string addHeadLineToCSV(string path)
+        public void connect()
         {
-            var csv = new StringBuilder();
-
-            int numOfColoumns = features.Count();
-            string headline = "";
-            int i;
-            for(i=0; i<numOfColoumns-1; i++)
-            {
-                headline += i.ToString() + ",";
-            }
-            headline += i.ToString();
-            csv.AppendLine(headline);
-
-
-            var reader = new StreamReader(path);
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                csv.AppendLine(line);
-
-            }
-            reader.Close();
-
-            string timesstamp = DateTime.Now.ToString("yyyyMMddHHmmss");
-            string newpath = (path.Split(new string[] { ".csv" }, StringSplitOptions.None))[0] + timesstamp + ".csv";
-            File.WriteAllText(newpath, csv.ToString());
-            return newpath;
-        }
-
-        private void removeHeadLineFromCSV(string path)
-        {
-            if (File.Exists(path))
-            {
-                File.Delete(path);
-            }
-        }
-
-
-        private void getDLLAnomalyReport()
-        {
-
-
-            IntPtr pDll = NativeMethods.LoadLibrary(dllPath);
-            if (pDll == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your DLL.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-            }
-
-            IntPtr pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "timeseries_Create");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-
-            timeseries_Create timeseries_create = (timeseries_Create)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(timeseries_Create));
-
-
-            pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Anomaly_Detecor_Create");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-            Anomaly_Detecor_Create simpleDetecor_create = (Anomaly_Detecor_Create)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(Anomaly_Detecor_Create));
-
-            pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Anomaly_Detecor_LearnNormal");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-
-            Anomaly_Detecor_LearnNormal simpleDetecor_learnNormal = (Anomaly_Detecor_LearnNormal)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(Anomaly_Detecor_LearnNormal));
-
-            pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Anomaly_Detecor_Detect");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-
-            Anomaly_Detecor_Detect simpleDetecor_detect = (Anomaly_Detecor_Detect)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(Anomaly_Detecor_Detect));
-
-
-            pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Anomaly_Detecor_getReportSize");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-            Anomaly_Detecor_getReportSize simpleDetecor_getreportSize = (Anomaly_Detecor_getReportSize)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(Anomaly_Detecor_getReportSize));
-
-            pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Anomaly_Detecor_getAnomaly");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-            Anomaly_Detecor_getAnomaly simpleDetecor_getanomaly = (Anomaly_Detecor_getAnomaly)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(Anomaly_Detecor_getAnomaly));
-
-            pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Anomaly_Detecor_getAnomalyString");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-            Anomaly_Detecor_getAnomalyString simpleDetecor_getanomalyString = (Anomaly_Detecor_getAnomalyString)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(Anomaly_Detecor_getAnomalyString));
-
-
-            pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Anomaly_Detecor_DeleteAnomaly");
-            if (pAddressOfFunctionToCall == IntPtr.Zero)
-            {
-                Console.WriteLine("There was a problem loading your function.");
-                NativeMethods.FreeLibrary(pDll);
-                return;
-
-
-            }
-
-            Anomaly_Detecor_DeleteAnomaly simpleDetecor_deleteAnomaly = (Anomaly_Detecor_DeleteAnomaly)Marshal.GetDelegateForFunctionPointer(
-            pAddressOfFunctionToCall,
-            typeof(Anomaly_Detecor_DeleteAnomaly));
-
-
-
-            // write a new file that is a copy of the given path but with healines to the coloumns.
-            // after we finish using it, we will erase it.
-            string newfileNormal = addHeadLineToCSV(regFlightPath);
-            string newfileAnomaly = addHeadLineToCSV(anomalyFlightPath);
-
-
-
-            IntPtr tsNormal = timeseries_create(newfileNormal);
-            IntPtr tsAnomaly = timeseries_create(newfileAnomaly);
-
-
-            IntPtr sd = simpleDetecor_create();
-            simpleDetecor_learnNormal(sd, tsNormal);
+            HelperClassForDLL helper = new HelperClassForDLL(dllPath, features.Count,regFlightPath,anomalyFlightPath);
+            anomalyReport = helper.getDLLAnomalyReport();
+            anomalyreportSize = anomalyReport.Count; 
             nextAnomalyIndex = 0;
-            IntPtr report = simpleDetecor_detect(sd, tsAnomaly);
-            anomalyreportSize = (simpleDetecor_getreportSize(report));
-
-
-            for (int i = 0; i < anomalyreportSize; i++)
-            {
-                // get the CharHandler
-                IntPtr anomaly = simpleDetecor_getanomaly(report, i);
-                // get the string from the char handler
-                IntPtr strAnomaly = simpleDetecor_getanomalyString(anomaly);
-                string stranomaly = Marshal.PtrToStringAnsi(strAnomaly);
-                anomalyReport.Add(stranomaly);
-                // free the allocated memory
-                simpleDetecor_deleteAnomaly(anomaly);
-
-            }
-            if(anomalyreportSize > 0)
+            if (anomalyreportSize > 0)
             {
                 nextAnomaly = anomalyReport[0];
                 string[] splitstr = nextAnomaly.Split('-');
                 anomalyTime = int.Parse(splitstr[2]);
             }
-
-
-            NativeMethods.FreeLibrary(pDll);
-            removeHeadLineFromCSV(newfileNormal);
-            removeHeadLineFromCSV(newfileAnomaly);
-
-        }
-
-
-        public void connect()
-        {
-            getDLLAnomalyReport();
+            else
+            {
+                anomalyTime = -1;
+            }
+            while (time > anomalyTime && nextAnomalyIndex < (anomalyreportSize - 1))
+            {
+                nextAnomalyIndex++;
+                nextAnomaly = anomalyReport[nextAnomalyIndex];
+                string[] splitstr = nextAnomaly.Split('-');
+                anomalyTime = int.Parse(splitstr[2]);
+            }
         }
 
         public void start()
         {
             // if the something in the dll didnt work,
             //or that there are no anomalies, there is no need to start this model.
-            if (anomalyReport.Count > 0)
-            {
+
                 new Thread(delegate ()
                 {
                     int length = dataArray.Length;
                     while (time != length && isStop != true)
                     {
-                        while (time == anomalyTime)
+                        if (anomalyReport.Count > 0)
                         {
-                            NotifyPropertyChanged("Alarm");
-                            if (nextAnomalyIndex < (anomalyreportSize - 1))
+                            while (time == anomalyTime)
+                            {
+                                NotifyPropertyChanged("Alarm");
+                                if (nextAnomalyIndex < (anomalyreportSize - 1))
+                                {
+                                    nextAnomalyIndex++;
+                                    nextAnomaly = anomalyReport[nextAnomalyIndex];
+                                    string[] splitstr = nextAnomaly.Split('-');
+                                    anomalyTime = int.Parse(splitstr[2]);
+
+                                }
+
+
+                            }
+                            while (time > anomalyTime && nextAnomalyIndex < (anomalyreportSize - 1) && time != length)
                             {
                                 nextAnomalyIndex++;
                                 nextAnomaly = anomalyReport[nextAnomalyIndex];
                                 string[] splitstr = nextAnomaly.Split('-');
                                 anomalyTime = int.Parse(splitstr[2]);
-
                             }
-
-
-                        }
-                        while (time > anomalyTime && nextAnomalyIndex < (anomalyreportSize - 1))
-                        {
-                            nextAnomalyIndex++;
-                            nextAnomaly = anomalyReport[nextAnomalyIndex];
-                            string[] splitstr = nextAnomaly.Split('-');
-                            anomalyTime = int.Parse(splitstr[2]);
                         }
                         Time++;
                         while (speed == 0)
@@ -467,41 +250,11 @@ namespace ADP2_Flight_Inspection_App
 
                 }).Start();
 
-            }
-
         }
 
         public void stop()
         {
         }
-
-
-        // functions from the DLL
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr timeseries_Create([MarshalAs(UnmanagedType.LPStr)] string path);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr Anomaly_Detecor_Create();
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate void Anomaly_Detecor_LearnNormal(IntPtr sd, IntPtr ts);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr Anomaly_Detecor_Detect(IntPtr sd, IntPtr ts);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate int Anomaly_Detecor_getReportSize(IntPtr report);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr Anomaly_Detecor_getAnomaly(IntPtr report, int index);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr Anomaly_Detecor_getAnomalyString(IntPtr cptr);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate void Anomaly_Detecor_DeleteAnomaly(IntPtr cptr);
-
-
+        
     }
 }
